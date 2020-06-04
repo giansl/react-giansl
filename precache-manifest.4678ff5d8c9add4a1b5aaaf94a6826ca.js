aa36a701955a8167fec38b8037cc808c57160b53
giansl <giansolis93@gmail.com>
2020-06-03 23:57:51 -0500

    Updates

---
+++ precache-manifest.4678ff5d8c9add4a1b5aaaf94a6826ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02c586976925896b22bb02893bfad06",
+    "url": "/react-giansl/index.html"
+  },
+  {
+    "revision": "96aa7dc28c563c21fe2e",
+    "url": "/react-giansl/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "86706c2fccccc7c91c74",
+    "url": "/react-giansl/static/css/main.2b7ad184.chunk.css"
+  },
+  {
+    "revision": "96aa7dc28c563c21fe2e",
+    "url": "/react-giansl/static/js/2.4bedd98d.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/react-giansl/static/js/2.4bedd98d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86706c2fccccc7c91c74",
+    "url": "/react-giansl/static/js/main.3d59c8cd.chunk.js"
+  },
+  {
+    "revision": "b120f809e3036237cecd",
+    "url": "/react-giansl/static/js/runtime-main.0cee9a81.js"
+  }
+]);
